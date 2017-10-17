b7a7ae8e08c9eab8c4497fe878e85c8517a05c5e
Aluno <isahernandes06@gmail.com>
2017-10-17 10:27:11 -0200

    Criação do projeto

---
+++ app/src/main/java/com/example/aluno/atividadebd/AtividadeBD.java
@@ -0,0 +1,13 @@
+package com.example.aluno.atividadebd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AtividadeBD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atividade_bd);
+    }
+}
